b4ed761702ceb09a26d5baa8ac1f396d7c8643a5
zydxt <zydxt1991@gmail.com>
2024-04-04 23:18:29 +0800

    support local llm model

--- install.py
+++ install.py
@@ -1,6 +1,11 @@
 import launch
 
 if not launch.is_installed("openai"):
-    launch.run_pip("install openai==1.10.0", "requirements RPG-DiffusionMaster")
+    launch.run_pip("install openai==1.10.0", "[RPG-DiffusionMaster] install openai")
 if not launch.is_installed("google-generativeai"):
-    launch.run_pip("install google-generativeai==0.3.2", "requirements RPG-DiffusionMaster")
+    launch.run_pip(
+        "install google-generativeai==0.3.2",
+        "[RPG-DiffusionMaster] install google-generativeai",
+    )
+if not launch.is_installed("transformers"):
+    launch.run_pip("install transformers", "[RPG-DiffusionMaster] install transformers")
--- rpg_lib/rpg_enums.py
+++ rpg_lib/rpg_enums.py
@@ -10,3 +10,10 @@ class LLMType(Enum):
     GEMINI_PRO = "gemini-pro"
     GPT4 = "gpt4"
     GPT4_AZURE = "GPT4-AZURE"
+    LOCAL_LLM = "Local-LLM"
+
+
+class Quantization(Enum):
+    Q_4_BIT = "4BIT"
+    Q_8_BIT = "8BIT"
+    NON_Q = "Non_quantization"
